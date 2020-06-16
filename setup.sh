cdddbd387af4b820fd92a184780077f70e382415
xcH3332 <brush123z@gmail.com>
2020-06-16 21:51:48 +0530

    Create setup.sh

---
+++ setup.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+echo -e "[+] Installing SNAP"
+sudo apt install snapd
+sudo systemctl start snapd
+sudo systemctl enable snapd
+echo -e "[+] Installing APPARMOR"
+sudo systemctl start apparmor
+sudo systemctl enable apparmor
+echo -e "[+] Installing MULTIPASS"
+sudo snap install multipass
+sudo systemctl restart snapd
+multipass
+echo -e "[+]Done! Now go bulding Ubuntu-VMs"
